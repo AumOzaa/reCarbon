ab4f9ea50f346e8a17f37e366306fc017ece3f81
Maharshi <maharshilavana@gmail.com>
2026-09-12 12:03:12 +0530

    added cors middleware

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
+const cors = require('cors')
 const connectDB = require('./config/db');
 const logger = require('./config/logger');
 const { initializePinecone } = require('./services/pinecone.service');
@@ -10,6 +11,12 @@ const startServer = async () => {
   try {
     // Connect to MongoDB
     await connectDB();
+
+    app.use(cors({
+        origin: ['http://localhost:3000', 'http://127.0.0.1:3000','http://127.0.0.1:3001','http://localhost:3001'],
+      credentials: true,
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }));
 
     // Initialize Pinecone for vector indexing
     await initializePinecone();
